359341099da26985295351bd4dea5d1bd9bcb700
Drew James <ProtoChoco@users.noreply.github.com>
2018-11-12 13:26:11 -0600

    Added files (awaiting pause fix)

---
+++ DeliveryBoy 0.01/Assets/PlayerScripts/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+}
+
+    private void Update(){
+        while (Input.GetKeyDown(KeyCode.Escape)){
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+       
+    }
+
+    void OnApplicationFocus(bool ApplicationIsBack) {
+        if (ApplicationIsBack == true) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
