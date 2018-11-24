1320b04c4a000bdf22c3b36cb2a94322d96f219f
Drew James <ProtoChoco@users.noreply.github.com>
2018-11-24 00:20:09 -0600

    Refining Controls (part 2 of 3)
    Improved camera script and smoothed out movement

---
+++ DeliveryBoy 0.03/Assets/EnemyScripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+/* Order of operations
+
+
+    - Enemy 'sees' player (player comes into raycast)
+    - Enemy moves within radius of player
+    - Enemy opens fire while circling perimeter (randomly change direction)
+
+*/
---
+++ DeliveryBoy 0.03/Assets/PlayerScripts/PlayerMove.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// The GameObject is made to bounce using the space key.
+// Also the GameOject can be moved forward/backward and left/right.
+// Add a Quad to the scene so this GameObject can collider with a floor.
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 playerMove = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        gameObject.transform.position = new Vector3(0, 5, 0);
+    }
+
+    void Update()
+    {
+        move();
+
+        if (Input.GetKey(KeyCode.Escape))
+            locker(true);
+        else
+            locker(false);
+    }
+
+    void move()
+    {
+
+        if (controller.isGrounded)
+        {
+
+            playerMove = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            playerMove = transform.TransformDirection(playerMove);
+            playerMove = playerMove * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                playerMove.y = jumpSpeed;
+            }
+        }
+
+        playerMove.y = playerMove.y - (gravity * Time.deltaTime);
+
+        controller.Move(playerMove * Time.deltaTime);
+    }
+
+    void locker(bool t)
+    {
+        if (t)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
